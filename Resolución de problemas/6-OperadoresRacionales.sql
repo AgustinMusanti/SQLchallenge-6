d5606f6e9c19f30b3f243402a5a00ef57df749b9
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2025-03-09 16:32:00 -0300

    >>

--- Resolución de problemas/6-OperadoresRacionales.sql
+++ Resolución de problemas/6-OperadoresRacionales.sql
@@ -21,14 +21,14 @@ DESCRIBE articulos;
       -- (1,'impresora','Epson Stylus C45',400.80,20);
       -- (2,'impresora','Epson Stylus C85',500,30);
       -- (3,'monitor','Samsung 14',800,10);
-	    -- (4,'teclado','ingles Biswal',100,50);
+      -- (4,'teclado','ingles Biswal',100,50);
       -- (5,'teclado','español Biswal',90,50);
       
 INSERT INTO articulos (codigo, nombre, descripcion, precio, cantidad)
 VALUES(1,'impresora','Epson Stylus C45',400.80,20),
       (2,'impresora','Epson Stylus C85',500,30),
       (3,'monitor','Samsung 14',800,10),
-	    (4,'teclado','ingles Biswal',100,50),
+      (4,'teclado','ingles Biswal',100,50),
       (5,'teclado','español Biswal',90,50);
 
 -- 5- Seleccione todos los registros de la tabla.
