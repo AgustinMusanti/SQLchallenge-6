216196b19fb093a3555e7b349250b2a77b45ec01
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2025-03-10 21:10:33 -0300

    >>

--- Resolución de problemas/7-BorradoDeRegistros.sql
+++ Resolución de problemas/7-BorradoDeRegistros.sql
@@ -26,7 +26,7 @@ DESCRIBE agenda;
  
  INSERT INTO agenda (apellido, nombre, domicilio, telefono)
  VALUES('Mores','Alberto','Colon 123',4234567),
-	     ('Torres','Juan','Avellaneda 135',4458787),
+       ('Torres','Juan','Avellaneda 135',4458787),
        ('Lopez','Mariana','Urquiza 333',4545454),
        ('Lopez','Jose','Urquiza 333',4545454),
        ('Peralta','Susana','Gral. Paz 1234',4123456);
